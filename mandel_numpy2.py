101c022eecda32303c506387f3e37ab49b69c36c
Ondřej Čertík <ondrej@certik.us>
2023-08-30 15:19:50 -0600

    Use the same numbers

--- mandel_numpy2.py
+++ mandel_numpy2.py
@@ -1,7 +1,8 @@
 import numpy as np
 
 ITERATIONS = 100
-DENSITY = 1000
+n = 16
+DENSITY = 4096 // n
 x_min, x_max = -2.68, 1.32
 y_min, y_max = -1.5, 1.5
 
@@ -10,9 +11,10 @@ x, y = np.meshgrid(np.linspace(x_min, x_max, DENSITY),
 c = x + 1j*y
 z = c.copy()
 fractal = np.zeros(z.shape, dtype=np.uint8) + 255
+#print(z.shape)
 
 for n in range(ITERATIONS):
-    print("Iteration %d" % n)
+#    print("Iteration %d" % n)
     mask = abs(z) <= 10
     z[mask] *= z[mask]
     z[mask] += c[mask]
