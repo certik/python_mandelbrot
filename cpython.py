25cb6726631e7d28a6f7a924e72d413b420ff3c9
Ondřej Čertík <ondrej@certik.us>
2023-08-30 14:26:28 -0600

    Add a CPython version

---
+++ cpython.py
@@ -0,0 +1,32 @@
+import numpy as np
+
+MAX_ITERS = 1000
+def mandelbrot_kernel(c):
+    z = c
+    nv = 0
+    for i in range(MAX_ITERS):
+        if abs(z) > 2:
+            break
+        z = z*z + c
+        nv += 1
+    return nv
+
+n = 512
+height = 4096 // n
+width = 4096 // n
+min_x = -2.0
+max_x = 0.47
+min_y = -1.12
+max_y = 1.12
+scale_x = (max_x - min_x) / width
+scale_y = (max_y - min_y) / height
+
+output = np.empty((height,width), dtype=np.float64)
+
+for h in range(height):
+    cy = min_y + h * scale_y
+    for w in range(width):
+        cx = min_x + w * scale_x
+        output[h,w] = mandelbrot_kernel(cx + 1j*cy)
+
+print(output)
